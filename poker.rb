ce0df0a299915ca1a334383493ecee4702db43af
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2015-11-20 20:50:46 +0900

    Add Player and Porker

---
+++ poker.rb
@@ -0,0 +1,31 @@
+require 'bundler'
+Bundler.require
+
+class Poker
+  attr_accessor :player1
+
+  def initialize
+    @player1 = Player.new
+  end
+end
+
+class Player
+  attr_accessor :hand
+
+  def initialize
+    @hand = Array.new(5)
+  end
+end
+
+RSpec.describe Poker do
+  describe '.new' do
+    it { expect(subject.player1).to be_a Player }
+  end
+end
+
+RSpec.describe Player do
+  describe '.new' do
+    it { expect(subject.hand).to be_a Array }
+    it { expect(subject.hand.size).to eq(5) }
+  end
+end
